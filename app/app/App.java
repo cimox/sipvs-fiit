31be04f09ac3f98d4622633981b0f239a01a1aae
cimox <matus.cimerman@gmail.com>
2016-10-11 19:17:11 +0200

    App main class

---
+++ app/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+
+// main class
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("I hate Java");
+
+        System.out.println("Validating XML file...");
+
+
+
+        System.out.println("Finished...");
+    }
+}
